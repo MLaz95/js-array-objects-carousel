0a3551f10d868dd777b6e1172183113bd3192232
marco <lazzari.marco.ml@gmail.com>
2024-02-14 15:01:24 +0100

    rimpiazzato ciclo for con ciclo forEach per sfruttare proprietá oggetti

--- script.js
+++ script.js
@@ -9,22 +9,38 @@ Al termine di questa fase ci ritroveremo con lo stesso slider stilato nella mile
 // tramite un ciclo for prendiamo ogni indirizzo delle immagini dall'array
 // per ognuno di essi andremo a creare un elemento img dentro lo slider
 
-const images = ["./img/01.webp", "./img/02.webp", "./img/03.webp", "./img/04.webp", "./img/05.webp"];
-console.log(images);
+const images = [
+    {
+        image: 'img/01.webp',
+        title: 'Marvel\'s Spiderman Miles Morale',
+        text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+    }, {
+        image: 'img/02.webp',
+        title: 'Ratchet & Clank: Rift Apart',
+        text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+    }, {
+        image: 'img/03.webp',
+        title: 'Fortnite',
+        text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+    }, {
+        image: 'img/04.webp',
+        title: 'Stray',
+        text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+    }, {
+        image: 'img/05.webp',
+        title: "Marvel's Avengers",
+        text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
+    }
+];
 
 
 // bersagliamo lo slider
 const sliderElement = document.getElementById("slider");
 
+images.forEach(function(singleImage, index){
+    sliderElement.innerHTML += `<img src="${singleImage['image']}" alt="immagine ${index + 1}">`;
+})
 
-// tramite un ciclo for prendiamo ogni indirizzo delle immagini dall'array
-for (let i = 0; i < images.length; i++) {
-
-    sliderElement.innerHTML += `<img src="./img/0${i + 1}.webp" alt="immagine ${i + 1}">`;
-
-    // per ognuno di essi andremo a creare un elemento img dentro lo slider
-
-}
 
 document.querySelector("#slider img:nth-of-type(1)").className = "active";
 
