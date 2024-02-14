39b8c95bb0ea4d7eb08aae44d40ec5e05b127214
marco <lazzari.marco.ml@gmail.com>
2024-02-14 16:00:22 +0100

    primo oggetto del carosello stampa correttamente

--- script.js
+++ script.js
@@ -14,19 +14,19 @@ const images = [
         image: 'img/01.webp',
         title: 'Marvel\'s Spiderman Miles Morale',
         text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
-    }, {
+    },{
         image: 'img/02.webp',
         title: 'Ratchet & Clank: Rift Apart',
         text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
-    }, {
+    },{
         image: 'img/03.webp',
         title: 'Fortnite',
         text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
-    }, {
+    },{
         image: 'img/04.webp',
         title: 'Stray',
         text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
-    }, {
+    },{
         image: 'img/05.webp',
         title: "Marvel's Avengers",
         text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
@@ -38,11 +38,40 @@ const images = [
 const sliderElement = document.getElementById("slider");
 
 images.forEach(function(singleImage, index){
-    sliderElement.innerHTML += `<img src="${singleImage['image']}" alt="immagine ${index + 1}">`;
+    // creates slide container
+    const newSlide = document.createElement('div');
+    newSlide.classList.add('slide');
+
+    // creates image element with relevant img
+    const slideImage = document.createElement('img');
+    slideImage.src = singleImage['image'];
+    slideImage.alt = `immagine ${index + 1}`;
+
+    // adds image to slide
+    newSlide.append(slideImage);
+
+    // creates text container
+    const slideInfo = document.createElement('div');
+    slideInfo.classList.add('text');
+    // title
+    const infoTitle = document.createElement('h3');
+    infoTitle.innerHTML = singleImage['title'];
+    // text
+    const infoText = document.createElement('p');
+    infoText.innerHTML = singleImage['text'];
+    // puts title and text in text container
+    slideInfo.append(infoTitle, infoText);
+
+    // puts text container in slide
+    newSlide.append(slideInfo)
+    // puts slide in page
+    sliderElement.append(newSlide)
+
 })
 
 
-document.querySelector("#slider img:nth-of-type(1)").className = "active";
+// selects the first .slide and gives it class active
+document.querySelector(".slide").classList.add('active');
 
 /*
 -  salvo un contatore della slide
